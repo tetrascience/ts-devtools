56fc44d48a92b8be2871a4492eaeb253412f86e3
Punya Biswal <pbiswal@tetrascience.com>
2020-11-12 12:25:29 -0500

    Add load_pipeline_config testing API

--- tetrascience/taskdev/testing.py
+++ tetrascience/taskdev/testing.py
@@ -3,6 +3,11 @@ import typing as t
 import os
 
 
+def load_pipeline_config(context: Context, config: t.Dict[str, str]):
+    """Load config into this context's pipeline config."""
+    context._pipeline_config = config
+
+
 def load_test_input(
     context: Context, local_path: os.PathLike, datalake_path: str, **kwargs
 ) -> File:
@@ -26,7 +31,7 @@ def check_test_output(
     local file. Optionally transforms the file contents before comparing
     (for example, to ignore whitespace and text ordering for JSON files).
 
-    >>> check_test_output(ctx, base_dir / 'expected.json', output_file, json.loads)    
+    >>> check_test_output(ctx, base_dir / 'expected.json', output_file, json.loads)
     """
     with open(local_path, "rb") as f:
         expected = f.read()
